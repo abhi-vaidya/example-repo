1ceacaf8dd89903e3d3f053e5825807e7fdcf98f
abhijit.vaidya <avaidya@dyvenia.com>
2024-02-05 17:35:14 +0100

    adding hello world

--- dags/hello_word.py
+++ dags/hello_word.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 5, 2),
+    'start_date': datetime(2024, 2, 5),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
